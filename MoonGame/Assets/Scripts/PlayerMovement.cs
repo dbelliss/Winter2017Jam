79f7a8bfc59f3a5ed2ec1927cd2dcddbc4e95825
dbelliss <daveybelliss@yahoo.com>
2017-01-28 13:16:30 -0800

    Player can jump and isGrounded

--- MoonGame/Assets/Scripts/PlayerMovement.cs
+++ MoonGame/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 	Rigidbody2D rb2d;
 
-	public float speed = 1;
-	public float movex;
-	// Use this for initialization
+	public float speed = 1; //Speed to move horizontally
+	public float MaxJumpTime = 2f; //Cooldown for jump
+	public float JumpForce; //Force of jump
+
+	public Transform isGround;
+
+	bool isGrounded = false;
+
+	private float movex; //Movement speed in x direction
+
+
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
 	}
-	
+
+	void Update() {
+
+	}
 	// Update is called once per frame
 	void FixedUpdate () {
 		movex = Input.GetAxis ("Horizontal");
 		rb2d.velocity = new Vector2(movex * speed, rb2d.velocity.y);
+
+
+		float movey = Input.GetAxis ("Vertical");
+		rb2d.AddForce (new Vector2 (0, movey * JumpForce));
+
+		isGrounded = Physics2D.Raycast(isGround.position, -Vector2.up, 0.1f);
+//		Debug.DrawLine(isGround.position, new Vector3 (isGround.position.x, -.5f,0), Color.red);
+//		Debug.Log (isGrounded);
 	}
+
 }
