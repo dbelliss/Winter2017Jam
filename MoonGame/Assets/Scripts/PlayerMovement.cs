f5295be1c1ebaf299c23d3e48fcc39c4609f83f2
dbelliss <daveybelliss@yahoo.com>
2017-01-28 12:45:54 -0800

    Player moves left and right

---
+++ MoonGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody2D rb2d;
+
+	public float speed = 1;
+	public float movex;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		movex = Input.GetAxis ("Horizontal");
+		rb2d.velocity = new Vector2(movex * speed, rb2d.velocity.y);
+	}
+}
